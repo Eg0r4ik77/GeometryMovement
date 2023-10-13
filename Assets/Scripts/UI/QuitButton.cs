9d3ed2abd90de3a526eebee9b98ea8ff330583b6
Egor Slesarenko <egor050503sl@gmail.com>
2023-10-13 20:22:42 +0300

    add quit button

---
+++ Assets/Scripts/UI/QuitButton.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class QuitButton : MonoBehaviour
+{
+    private Button _quitButton;
+
+    private void Awake()
+    {
+        _quitButton = GetComponent<Button>();
+    }
+
+    private void OnEnable()
+    {
+        _quitButton.onClick.AddListener(Quit);
+    }
+
+    private void OnDisable()
+    {
+        _quitButton.onClick.RemoveListener(Quit);
+    }
+
+    private void Quit()
+    {
+        Application.Quit();
+        print("Quit");
+    }
+}
